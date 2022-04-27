a5503648b8bde5481db54cc686db8b796971b94d
StefanoMast <ste.mastrantonio@gmail.com>
2022-04-27 15:23:35 +0200

    add pseudo codice js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+Consigli del giorno:
+* Pensate prima in italiano.
+* Dividete in piccoli problemi la consegna.
+* Individuate gli elementi di cui avete bisogno per realizzare il programma.
+Buon lavoro ragazzi :tada: :muscolo:*/
+
+/*[] Genero cinque numeri random
+     - uso la funzione math.floor
+     - faccio un array
+
+[] far partire il timer di 30 secondi
+     - al termine del timer, faccio scomparire i numeri 
+
+[] chiedere all'utente per cinque volte quali sono i numeri che ha visto
+    - chiedo all'utente tramite prompt i numeri che ha visto
+
+[] dopo che sono stati inseriti i numeri, il fotware dice quanti e quali dei numeri da indovinare sono stati individuati
+    - salvo i numeri dell'utente in un array
+    - con i due array che ho creato, metto come true i numeri presenti in entrambi e false quelli che non lo sono
