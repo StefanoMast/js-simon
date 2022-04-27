9835d08b053b9c8393c2e29b2002cd86503fa994
StefanoMast <ste.mastrantonio@gmail.com>
2022-04-27 16:15:42 +0200

    add funzione numeri random

--- js/script.js
+++ js/script.js
@@ -19,4 +19,28 @@ Buon lavoro ragazzi :tada: :muscolo:*/
 
 [] dopo che sono stati inseriti i numeri, il fotware dice quanti e quali dei numeri da indovinare sono stati individuati
     - salvo i numeri dell'utente in un array
-    - con i due array che ho creato, metto come true i numeri presenti in entrambi e false quelli che non lo sono
+    - con i due array che ho creato, metto come true i numeri presenti in entrambi e false quelli che non lo sono*/
+
+//creo un array di cinque numeri casuali
+const arrayRandom = numeriRandom(5, 100);
+console.log(arrayRandom);
+/*Funzione per riempire l'array con  */
+function numeriRandom(maxElement, maxRange) {
+    const arrayNumeriRandom =[];
+    let index = 0;
+    //finché il contatore è minore del numero massimo di elementi
+    while (index < maxElement) {
+        let numeroRandom = getRndInteger (1, maxRange);
+        //se genera numeri uguali vanno scartati
+        if (!arrayNumeriRandom.includes(numeroRandom)) {
+            arrayNumeriRandom.push(numeroRandom)
+            index++;
+        }
+    }
+    return arrayNumeriRandom;
+}
+//funzione per generare numeri casuali
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
